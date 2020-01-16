5c8ae79370dd56b208981204be1c514199d1a3cf
Václav Kubernát <sir.venceslas@gmail.com>
2020-01-16 20:55:26 +0100

    Update usage

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Obědy Dejvice
 
 Scraper jídelníčků dejvických jídelen.
+
+## Použití
+Skript reaguje na `argv[0]` čili název skriptu. Ke změně názvu skriptu lze použít například symbolický link (nebo příkaz `cp`)(nebo příkaz `exec -a`)(možností je hodně).
--- obedy.py
+++ obedy.py
@@ -66,7 +66,7 @@ if 'blox' in sys.argv[0]:
 elif 'country' in sys.argv[0]:
     menu = country_life()
 else:
-    print('Název skriptu musí obsahovat jedno z těchto slov: "blox", "country"')
+    print('Název skriptu musí obsahovat jedno z těchto slov: "blox", "country"\nPoužijte symbolický odkaz k pojmenování skriptu.')
     exit(1)
 
 if len(sys.argv) >= 2:
