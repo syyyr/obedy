72007cf695f74ae493ee4264f5e02060b5ad160e
Václav Kubernát <sir.venceslas@gmail.com>
2020-01-27 13:22:11 +0100

    Include date in printout

--- obedy.py
+++ obedy.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 from bs4 import BeautifulSoup
-from datetime import date, datetime
+from collections import OrderedDict
+from datetime import date, datetime, timedelta
 import locale
 import re
 import requests
@@ -18,37 +19,58 @@ def blox():
     page = requests.get('http://www.blox-restaurant.cz/#!/page_obedy')
     soup = BeautifulSoup(page.content, 'html.parser')
     allTr = iter(soup.find(id='page_obedy').findAll('tr'))
-    next(allTr) # Skip "Menu na týden..."
 
-    res = {}
+    res = OrderedDict()
+    match_date = re.match('Menu na týden (\d+)\.(\d+)\. - (\d+).(\d+)\.(\d+)', next(allTr).find('strong').text)
+    current_date = date(int(match_date.group(5)), int(match_date.group(2)), int(match_date.group(1)))
+    res[current_date] = []
+    next(allTr) # Skip first - it's the day tag - to prevent advancing the date too soon
 
     for item in allTr:
         day_tag = item.find('strong')
         if day_tag is not None:
-            current_day = re.sub(r'/.*$', '', day_tag.text)
-            res[current_day] = []
+            current_date = current_date + timedelta(days=1)
+            res[current_date] = []
             continue
 
         meals = item.find_all('td')
         if '\xa0' in meals[3].text: # Nonsense price -> we're already at the end
             break
-        res[current_day].append({ 'name': meals[1].text, 'price': meals[3].text })
+        res[current_date].append({ 'name': meals[1].text, 'price': meals[3].text })
 
     return ('Blox', res)
 
 def country_life():
+    monthToInt = {
+        'ledna': 1,
+        'února': 2,
+        'března': 3,
+        'dubna': 4,
+        'května': 5,
+        'června': 6,
+        'července': 7,
+        'srpna': 8,
+        'září': 9,
+        'října': 10,
+        'listopadu': 11,
+        'prosince': 12,
+    }
     page = requests.get('https://www.countrylife.cz/mo-dejvice-restaurace')
     soup = BeautifulSoup(page.content, 'html.parser')
     menu = soup.find(text='Jídelní lístek na tento týden:').findNext('div').findAll('p')
-
-    res = {}
-
+    res = OrderedDict()
     for item in menu:
         if item.text == '': # There are some bogus <p> elements
             continue
+
         day_tag = item.find('strong')
-        current_day = re.sub(' .*$', '', day_tag.text) # Discard the date from the weekday
-        res[current_day] = []
+        if day_tag.text == 'Alergeny:': # This is the end
+            break
+        match_date = re.match('.* (\d+)\. ([a-z]+)', day_tag.text)
+
+        # Yeah, the year won't work at the end of the year, but I don't really care
+        current_date = date(date.today().year, monthToInt[match_date.group(2)], int(match_date.group(1)))
+        res[current_date] = []
         meals = item.text.split('\n')[1:] # Discard the first element - it's the day
         for count, meal in enumerate(meals):
             meal = re.sub('doporučujeme|NOVINKA', '', meal) # I don't care about this stuff
@@ -59,7 +81,8 @@ def country_life():
             name = re.sub(' $', '', name)
 
             price = '39 Kč/porce' if count == 0 else '22 Kč/100 g' if datetime.now().hour > 16 else '27 Kč/100 g'
-            res[current_day].append({ 'name': name, 'price': price })
+            res[current_date].append({ 'name': name, 'price': price })
+
 
     return ('Country life', res)
 
@@ -67,18 +90,22 @@ def husa():
     page = requests.get('http://www.potrefene-husy.cz/cz/dejvice-poledni-menu')
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    res = {}
+    res = OrderedDict()
+    header_with_date = soup.find('h2')
+    match_date = re.match('od (\d+)\.(\d+)\.(\d+)', header_with_date.text)
+    current_date = date(int(match_date.group(3)), int(match_date.group(2)), int(match_date.group(1)))
 
     monday_tag = soup.find('tr', text='Pondělí')
-    current_day = 'Pondělí'
-    res[current_day] = []
+    res[current_date] = []
 
     menu = monday_tag.findAllNext('tr')
     for item in menu:
         day_tag = item.find('h3')
         if day_tag is not None:
-            current_day = day_tag.text
-            res[current_day] = []
+            if current_date.weekday() == 4: # Friday - end
+                break
+            current_date = current_date + timedelta(days=1)
+            res[current_date] = []
             continue
         tds = item.findAll('td')
         if len(tds) == 0: # bogus element between days
@@ -89,7 +116,7 @@ def husa():
         name = name.replace('\t', '') # bogus tab in name
         name = re.sub(' +', ' ', name)
         price = tds[2].text
-        res[current_day].append({ 'name': name, 'price': price })
+        res[current_date].append({ 'name': name, 'price': price })
 
     return ('Potrefená husa', res)
 
@@ -104,27 +131,27 @@ def main():
         print('Název skriptu musí obsahovat jedno z těchto slov: "blox", "country"\nPoužijte symbolický odkaz k pojmenování skriptu.')
         exit(1)
 
+    locale.setlocale(locale.LC_TIME, 'cs_CZ.UTF-8') # You better have this locale installed lmao
     if len(sys.argv) >= 2:
-        day = str(sys.argv[1])
-        if len(day) == 2:
-            day = {'po': 'Pondělí', 'út': 'Úterý', 'st': 'Středa', 'čt': 'Čtvrtek', 'pá': 'Pátek',
-                                    'ut': 'Úterý',                 'ct': 'Čtvrtek', 'pa': 'Pátek', }[day]
+        weekdayStr = str(sys.argv[1])
+        weekday = {'po': 0, 'út': 1, 'st': 2, 'čt': 3, 'pá': 4, 'ut': 1, 'ct': 3, 'pa': 4}[weekdayStr]
     else:
-        locale.setlocale(locale.LC_TIME, 'cs_CZ.UTF-8')
-        day = date.today().strftime('%A')
+        weekday = date.today().weekday()
 
-    if day not in menu:
-        print('Neznámý den: "' + day + '". Podporované formáty: Pátek|pá|pa')
+    if weekday is None:
+        print('Neznámý den: "' + weekdayStr + '". Podporované formáty: Pátek|pá|pa')
         exit(1)
 
-    name_width = max(len(max(menu[day], key=lambda index: len(index['name']))['name']), len('Název'))
-    price_width = max(len(max(menu[day], key=lambda index: len(index['price']))['price']), len('Cena'))
+    (menu_date, menu) = list(menu.items())[weekday]
+
+    name_width = max(len(max(menu, key=lambda index: len(index['name']))['name']), len('Název'))
+    price_width = max(len(max(menu, key=lambda index: len(index['price']))['price']), len('Cena'))
     format_string = '{{}}  {{:{}}} {{:>{}}}'.format(name_width + 1, price_width + 1)
 
-    print(BOLD + restaurant + NORMAL + ' ' + ITALIC + GREY + day + NORMAL)
+    print(BOLD + restaurant + NORMAL + ' ' + ITALIC + GREY + menu_date.strftime('%A') + ' ' + menu_date.strftime('%d. %B') + NORMAL)
     print(DOUBLE_UNDERLINE + BLUE + format_string.format('#', 'Název', 'Cena') + NORMAL)
 
-    for count, meal in enumerate(menu[day]):
+    for count, meal in enumerate(menu):
         print(format_string.format(BLUE + str(count + 1) + NORMAL, meal['name'], meal['price']))
 
 if __name__ == '__main__':
