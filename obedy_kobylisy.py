a9c46cc5a95be7304a4ccc644488e0579f0e5ff4
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-02 11:41:21 +0200

    Add correction

--- obedy_kobylisy.py
+++ obedy_kobylisy.py
@@ -67,7 +67,7 @@ def impl_menicka(restaurant_id, correction_func):
             meal_name_tag = meal_tag.find('td', attrs={'class': 'food'})
             meal_name = meal_name_tag.text
             # Sometimes, there are bogus rows.
-            if meal_name in ('Polévka', 'Hlavní Jídla', 'Hlavní jídla', 'Hlavní jídla:', 'Hlavní jídla :', 'Speciality:', 'Specialita:', 'Specialita', 'Dezerty'):
+            if meal_name in ('Polévka', 'Hlavní Jídla', 'Hlavní Jidla', 'Hlavní jídla', 'Hlavní jídla:', 'Hlavní jídla :', 'Speciality:', 'Specialita:', 'Specialita', 'Dezerty'):
                 continue
             # Get rid of unnecessary information about the meal.
             meal_name = re.sub(r'\d+g', '', meal_name) # g
