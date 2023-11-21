f1b9cad568a0c0113e7e05a43f7c6f8cd40c8cc7
Václav Kubernát <sir.venceslas@gmail.com>
2023-11-21 10:07:20 +0100

    Add correction

--- obedy_kobylisy.py
+++ obedy_kobylisy.py
@@ -255,6 +255,7 @@ def soucku():
         name = re.sub(r'^Menu (\d)([^:])', r'Menu \1:\2', name)
 
         name = re.sub(r'^(\S)', lambda m: m.group(1).upper(), name)
+        name = re.sub(r'^Specialita(\S)', lambda m: m.group(1).upper(), name)
 
         # Sometimes, two meals are on the same row.
         dual_entry_match = re.match(r'(.+) (\d{2,}) (.+)', name)
